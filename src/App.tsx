6477014162cf7843f9e278a6606cd683a43eb412
parham-devdocs <p.77.pazargadi@gmail.com>
2025-01-26 20:34:04 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+// import 'antd/dist/antd.css'; // Ensure Ant Design CSS is imported
+import { Table } from "antd";
+
+const dataSource = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+function App() {
+  return (
+    <div >
+<Table dataSource={dataSource} columns={columns} />
+<div ></div>
+    </div>
+  );
+}
+
+export default App;
